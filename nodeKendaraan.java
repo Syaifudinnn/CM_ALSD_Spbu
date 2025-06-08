6ffe8c4a81d766610c038d3af64c88d7c72f3314
m-fatahillah <fatahcrown789@gmail.com>
2025-06-08 19:33:32 +0700

    neewwww

--- nodeKendaraan.java
+++ nodeKendaraan.java
@@ -2,10 +2,14 @@ public class nodeKendaraan {
 
     //Class ini digunakan utk node linked list antrian kendaraan dan menyimpan objek kendaraan dan pointer node
 
-    //attribut
-    //kerjakan disini
+    // atribut
+    kendaraan data;
+    nodeKendaraan next;
 
-    //konstruktor
-    //kerjakan disini
+    // konstruktor
+    public nodeKendaraan(kendaraan data) {
+        this.data = data;
+        this.next = null;
+    }
 
 }
